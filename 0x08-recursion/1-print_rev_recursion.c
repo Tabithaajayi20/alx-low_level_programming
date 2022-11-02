fcd376f683d5fc2d8a0aba5daae9f3191dd1210f
Tabithaajayi20 <ayodelicate2022@gmail.com>
2022-11-02 07:11:56 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints string in reverse form
+ * @s: string to be printed
+ * Return: 0
+*/
+void _print_rev_recursion(char *s)
+{
+	_print_rev_recursion("print in a reverse mode");
+	return (0);
+}
